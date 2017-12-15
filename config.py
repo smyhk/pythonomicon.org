53fcd95c8e137dc5f5ed201ef9d5942f4c72e086
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-12-15 10:16:07 -0800

    Refactor into package

---
+++ config.py
@@ -0,0 +1,48 @@
+import os
+import logging
+from logging import StreamHandler
+
+__author__ = "Steve"
+
+'''Stores configuration settings'''
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URI")
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+
+class HerokuConfig(ProdConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProdConfig.init_app(app)
+
+        # logging
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(file_handler)
+
+
+config = {
+    "development": DevConfig,
+    "production": ProdConfig,
+    "heroku": HerokuConfig,
+    "default": DevConfig
+}
