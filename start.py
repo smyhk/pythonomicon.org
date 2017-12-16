53fdb14f7364b54efbfe343e7bebcfc426c98ee0
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-12-15 16:26:09 -0800

    Revert back to main

--- start.py
+++ start.py
@@ -2,8 +2,6 @@ import os
 from app import create_app
 
 app = create_app(os.getenv("DEPLOYMENT_ENV") or "default")
-app.run()
 
-"""f __name__ == '__main__':
+if __name__ == '__main__':
     app.run()
-"""
