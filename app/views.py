a255f04f342381338dc71dbb30f50f1d6f5e6a93
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-12-19 15:06:29 -0800

    Remove unused import and cleanup code

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
 from flask import render_template, redirect, flash, request, url_for, abort
-from flask_login import login_user, login_required, logout_user, current_user
+from flask_login import login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import login_manager
-from app.models import db, User
+from app.models import db
 from app.forms import LoginForm, SignupForm
 from urllib.parse import urlparse, urljoin
 
@@ -12,7 +12,7 @@ mod = Blueprint("app", __name__, template_folder="templates", static_folder="sta
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.User.query.get(int(user_id))
 
 
 @mod.route("/")
@@ -31,16 +31,16 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = db.User.query.filter_by(username=form.username.data).first()
         if user:
             if check_password_hash(user.password, form.password.data):
                 login_user(user)
                 flash("Logged in successfully")
 
-                next = request.args.get('next')
+                dest = request.args.get('next')
                 # is_safe_url should check if the url is safe for redirects.
                 # See http://flask.pocoo.org/snippets/62/ for an example.
-                if not is_safe_url(next):
+                if not is_safe_url(dest):
                     return abort(400)
 
                 return redirect(next or url_for('app.index'))
@@ -54,7 +54,7 @@ def signup():
 
     if form.validate_on_submit():
         hashed_password = generate_password_hash(form.password.data, method="sha256")
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        new_user = db.User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
         return redirect(url_for('app.index'))
