cb29cb8664f15ed56e0a8d9899f905116542cfc6
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-12-18 19:46:57 -0800

    Refactor migrate

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 import os
 from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate, MigrateCommand, upgrade
 from app import create_app
 from app.models import db
 
@@ -10,6 +10,11 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 migrate = Migrate(app, db)
+
+
+@manager.command
+def deploy():
+    upgrade()
 
 
 if __name__ == '__main__':
